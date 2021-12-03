5d69af95985d871bf6e0780392a3870f0bc7ef95
filip.zarnowiec <Pomelo2752>
2021-12-03 01:27:24 +0100

    Unit test directory

---
+++ app/src/test/java/pl/pw/mierzopuls/ImageProcessingTest.kt
@@ -0,0 +1,11 @@
+package pl.pw.mierzopuls
+
+import org.junit.Assert.fail
+import org.junit.Test
+
+class ImageProcessingTest {
+    @Test
+    fun `correct min, max, mean calculation`() {
+        fail("not implemented!")
+    }
+}
