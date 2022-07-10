2ff69bc0125643926323c704daf5541df5f040c3
LeCadavrExquis <filipzarnowiec@gmail.com>
2022-07-10 15:11:37 +0200

    Send fps param at the end of study

--- app/src/main/java/pl/pw/mierzopuls/model/Events.kt
+++ app/src/main/java/pl/pw/mierzopuls/model/Events.kt
@@ -2,13 +2,23 @@ package pl.pw.mierzopuls.model
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.analytics.FirebaseAnalytics
+
+const val STUDY_EVENT = "event_study"
 
 fun sendEvent(context: Context, study: Study) {
     FirebaseAnalytics.getInstance(context).logEvent(
-        "event_study", Bundle().apply {
+        STUDY_EVENT, Bundle().apply {
             putInt("pulse", study.pulse)
             putString("date", study.date)
+            putInt("fps", study.fps())
         }
     )
+    Log.d(STUDY_EVENT, """
+        params:
+        date = ${study.date}
+        pulse = ${study.pulse}
+        fps = ${study.fps()}
+    """.trimIndent())
 }
